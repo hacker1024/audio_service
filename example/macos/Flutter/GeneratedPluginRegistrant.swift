baa242aba0899919d097fac7419e83512b9bc8d8
hacker124 <hacker1024@users.sourceforge.net>
2020-09-04 13:54:41 +1000

    Generate macOS example workspace; bump the macOS version to 10.12.2

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import audio_session
 import just_audio
 import path_provider_macos
 import sqflite
+import audio_service
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
 }
--- macos/audio_service.podspec
+++ macos/audio_service.podspec
@@ -16,7 +16,7 @@ Flutter plugin to play audio in the background while the screen is off.
   s.source_files     = 'Classes/**/*'
   s.dependency 'FlutterMacOS'
 
-  s.platform = :osx, '10.11'
+  s.platform = :osx, '10.12.2'
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES' }
   s.swift_version = '5.0'
 end
